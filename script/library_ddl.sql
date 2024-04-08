36933fc8f9e3418306077b3079b5d6b53d53fbea
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-04-09 01:36:43 +0500

    Update library_ddl.sql

--- script/library_ddl.sql
+++ script/library_ddl.sql
@@ -15,7 +15,7 @@ CREATE TABLE Departments(
 CREATE TABLE Books (
       book_id INTEGER NOT NULL PRIMARY KEY,
       FOREIGN KEY (department_id) REFERENCES Departments(department_id),
-      title VARSHAR(200) NOT NULL,
+      title VARCHAR(200) NOT NULL,
       author VARCHAR(200) NOT NULL,
       volume INTEGER NOT NULL,
       publishing_year INTEGER NOT NULL,
