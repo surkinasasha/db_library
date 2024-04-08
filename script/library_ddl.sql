241cf2a3b215a30df80fd9dbc6c12d4a8b98f09b
surkinasasha <139327592+surkinasasha@users.noreply.github.com>
2024-04-08 23:40:41 +0500

    Update library_ddl.sql

--- script/library_ddl.sql
+++ script/library_ddl.sql
@@ -7,17 +7,17 @@ CREATE TABLE Readers (
 
 CREATE TABLE Departments(
       department_id INTEGER NOT NULL PRIMARY KEY,
-      dep_name VARCHAR(200) NOT NULL,
+      dep_name VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Books (
       book_id INTEGER NOT NULL PRIMARY KEY,
       FOREIGN KEY (department_id) REFERENCES Departments(department_id),
       title VARSHAR(200) NOT NULL,
-      author NOT NULL VARCHAR(200),
+      author VARCHAR(200) NOT NULL,
       volume INTEGER NOT NULL,
       publishing_year INTEGER NOT NULL,
-      status VARCHAR(20) NOT NULL,
+      status VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Reservation(
@@ -25,20 +25,20 @@ CREATE TABLE Reservation(
       FOREIGN KEY (reader_id) REFERENCES Readers(reader_id),
       FOREIGN KEY (book_id) REFERENCES Books(book_id),
       reserv_date DATE NOT NULL,
-      end_of_reservation DATE NOT NULL,
+      end_of_reservation DATE NOT NULL
 );
 
-CREATE TABLE Accounting of books(
+CREATE TABLE Accounting_of_books(
       movement_id INTEGER NOT NULL PRIMARY KEY,
       FOREIGN KEY (reader_id) REFERENCES Readers(reader_id),
       FOREIGN KEY (book_id) REFERENCES Books(book_id),
       issue_date DATE NOT NULL,
       return_deadline_date DATE NOT NULL,
-      return_date DATE,
+      return_date DATE
 );
 
 CREATE TABLE Debts(
-      FOREIGN KEY (movement_id) REFERENCES Accounting of books(movement_id),
+      FOREIGN KEY (movement_id) REFERENCES Accounting_of_books(movement_id),
       FOREIGN KEY (reader_id) REFERENCES Readers(reader_id),
-      status VARCHAR(20) NOT NULL,
+      status VARCHAR(20) NOT NULL
 );
